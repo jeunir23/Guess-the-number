551810dd7720d9979a8f27aa1e1277ff370e68b8
jeunir23 <jeunir23@gmail.com>
2024-05-21 14:59:25 +0800

    add eventlistener click function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
 let score = 20;
 let highscore = 0;
+
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // No number input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No input number 🥹';
+  }
+});
